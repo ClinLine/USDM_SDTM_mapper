f7f38918e1760d2cdaef29ec34acf0f1e0dd9be8
noah <143404345+NoahBrezet@users.noreply.github.com>
2025-06-06 12:59:05 +0000

    commit

--- reed.py
+++ reed.py
@@ -3,24 +3,23 @@ import json
 import openpyxl
 import pandas as pd
 
-with open('ReCoPad.json', 'r') as file:
-    data = json.load(file)
-
-expr = jsonata.Jsonata("study.versions.studyDesigns.blindingSchema.standardCode.decode")
-result = expr.evaluate(data)
-print(result)
-
 wb = openpyxl.load_workbook("Maps/sdtm_mapping_paths.xlsx")
 
 # Access the 'TS Parameters' sheet
 ts_sheet = wb['TS Parameters']
 
 # Print the value in the first and seventh column of each row in the 'TS Parameters' sheet
-for i in range(1, ts_sheet.max_row + 1):
-    c1 = ts_sheet.cell(row=i, column=1).value
-    print(c1)
-    print(ts_sheet.cell(row=i, column=7).value)
-    if ts_sheet.cell(row=i, column=7).value is not None:
-        ts_sheet.cell(row=i, column=8).value = c1
-    print(ts_sheet.cell(row=i, column=8).value)
+with open('ReCoPad.json', 'r') as file:
+    data=json.load (file)
+    for i in range(2, ts_sheet.max_row + 1):
+        codeSnip = ts_sheet.cell(row=i, column=7).value
+        try:
+            expr = jsonata.Jsonata(codeSnip)
+            result = expr.evaluate(data)
+            result2 = result.replace("’", " ")
+        except:
+            result2 = "No result"
+        if result2 is not None:
+            ts_sheet.cell(row=i, column=8).value = result2
+            print(result2)
 wb.save("Maps/sdtm_mapping_paths.xlsx")
