90a817114ad7b58f28e7320ad68da274abaa3704
bsnoeijer <138678352+bsnoeijer@users.noreply.github.com>
2025-06-06 15:52:00 +0000

    Code Update with error message for jsonata code

--- Read0.py
+++ Read0.py
@@ -14,23 +14,25 @@ ts_sheet = wb['TS Parameters']
 # Print the value in the first and seventh column of each row in the 'TS Parameters' sheet
 with open(JsonInput, 'r') as file:
     data=json.load (file)
-    ts_sheet.cell(row=1, column=7).value = "Mapping Result"
+    ts_sheet.cell(row=1, column=7).value = "Mapping Results"
     for i in range(2, ts_sheet.max_row + 1):
+        MapName = ts_sheet.cell(row=i, column=1).value
         codeSnip = ts_sheet.cell(row=i, column=7).value
-        
-        result2=" "
-        try:
-            expr = jsonata.Jsonata(codeSnip)
-            result = expr.evaluate(data)            
-        except:
-            result = None
-        # print(result)
+        if codeSnip is None:
+            result=" "
+        else:
+            try:
+                expr = jsonata.Jsonata(codeSnip)
+                result = expr.evaluate(data)            
+            except:
+                result = "Error in expression for "+ MapName + ": " + codeSnip
+        print(result)
         result= str(result)
         try:
             result2 = result.replace("’", " ")
         except:
             result2 = None
-        if result is None: result2= " "
+        if result2 is None: result2= " "
         if result2 is not None:
             ts_sheet.cell(row=i, column=7).value = result2            
          #   print(result2)
