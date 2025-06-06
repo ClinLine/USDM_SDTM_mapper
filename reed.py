6d76eaac652584e9b590c0ea15ea02ec2f5ef2f5
noah <143404345+NoahBrezet@users.noreply.github.com>
2025-06-06 11:11:29 +0000

    excel changing

--- reed.py
+++ reed.py
@@ -1,6 +1,8 @@
 import jsonata
 import json
+import openpyxl
 import pandas as pd
+
 with open('ReCoPad.json', 'r') as file:
     data = json.load(file)
 
@@ -8,5 +10,15 @@ expr = jsonata.Jsonata("study.versions.studyDesigns.blindingSchema.standardCode.
 result = expr.evaluate(data)
 print(result)
 
-df = pd.read_excel('your_file.xlsx')
-print(df)
+wb = openpyxl.load_workbook("Maps/sdtm_mapping_paths.xlsx")
+
+# Access the 'TS Parameters' sheet
+ts_sheet = wb['TS Parameters']
+
+# Print the value in the first and seventh column of each row in the 'TS Parameters' sheet
+for i in range(1, ts_sheet.max_row + 1):
+    print(ts_sheet.cell(row=i, column=1).value)
+    print(ts_sheet.cell(row=i, column=7).value)
+    ts_sheet.cell(row=i, column=8).value = "no data"
+    print(ts_sheet.cell(row=i, column=8).value)
+wb.save("Maps/sdtm_mapping_paths.xlsx")
