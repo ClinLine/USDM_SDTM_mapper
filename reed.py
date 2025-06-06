d9f4b15b6dd451d77252426e1d74d2c08386c0a3
noah <143404345+NoahBrezet@users.noreply.github.com>
2025-06-06 09:41:01 +0000

    Pending changes exported from your codespace

--- reed.py
+++ reed.py
@@ -1,6 +1,12 @@
 import jsonata
 import json
+import pandas as pd
 with open('ReCoPad.json', 'r') as file:
     data = json.load(file)
 
-print(data)
+expr = jsonata.Jsonata("study.versions.studyDesigns.blindingSchema.standardCode.decode")
+result = expr.evaluate(data)
+print(result)
+
+df = pd.read_excel('your_file.xlsx')
+print(df)
