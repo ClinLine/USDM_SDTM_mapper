bca31fd8669e02a1c25646928988c4f9cc7e6901
noah <143404345+NoahBrezet@users.noreply.github.com>
2025-06-20 16:43:12 +0000

    debuggidy

--- Main.py
+++ Main.py
@@ -6,11 +6,11 @@ import TS
 import TI
 
 # Define the source json file you like to use
-JsonInput = "TestJson/ReCoPad.json"
+JsonInput = "TestJson/EliLilly_NCT03421379_Diabetes.json"
 # define the mapping input file
 MapInput = "Maps/sdtm_mapping_paths.xlsx"
 # Define the resulting output file
-Output = "TestJson/SDTM_Results.xlsx"
+Output = "Output/SDTM_Results.xlsx"
 
 if __name__ == "__main__":
     # Create the TS sheet based on the mapping and the json input
@@ -18,3 +18,4 @@ if __name__ == "__main__":
     TS.Create_TS(wb, JsonInput)
     TI.Create_TI(wb, JsonInput)
     wb.save(Output)
+    wb.close()
--- TI.py
+++ TI.py
@@ -24,7 +24,7 @@ def get_ID(ID_string):
 def string_to_list(input, result):
     n = 0 #letter it is looking at
     while input[n] != "}": #looking for the end of the list
-        if input[n-2:n+2] == "', '" or input[n] in ["{"]: # looking for the start of a new item in the list
+        if input[n-2:n+2] == "', '" or input[n-3:n+2] == "' , '" or input[n] in ["{"]: # looking for the start of a new item in the list
             n += 1
             m = n
             while m+1 < len(input) and input[m+1] not in ("}") and input[m:m+3] not in ("', '"): # looking for the end of the item
