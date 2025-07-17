a9cdc1092c30f5718ecaa39aa764897dc6abfdfd
noah <143404345+NoahBrezet@users.noreply.github.com>
2025-07-17 14:29:45 +0000

    TV sort visits

--- TV.py
+++ TV.py
@@ -19,6 +19,9 @@ def Create_TV(wb, JsonInput):
         elif i == 3: # varName == "DOMAIN":
             DomainResult =  ta_sheet.cell(row=i, column=8).value
             DomainColumn = j
+        elif i == 4: # varName == "VISITNUM":
+            VisitNumCodeSnip = ta_sheet.cell(row=i, column=7).value
+            VisitNumColumn = j
         elif i == 5: # varName == "VISIT":
             VisitCodeSnip = ta_sheet.cell(row=i, column=7).value
             VisitDescriptionColumn = j
@@ -49,19 +52,23 @@ def Create_TV(wb, JsonInput):
     with open(JsonInput, 'r') as file:
         data=json.load (file)
         x = 1
-        studyId=definition.Parse_jsonata(codeSnip=StudyIdCodeSnip,data=data)         
+        studyId=definition.Parse_jsonata(codeSnip=StudyIdCodeSnip,data=data)
+        VisitNum=definition.Parse_jsonata(codeSnip=VisitNumCodeSnip,data=data)         
         Visit=definition.Parse_jsonata(codeSnip=VisitCodeSnip,data=data)
         Visitdy=definition.Parse_jsonata(codeSnip=VisitdyCodeSnip,data=data)
         Armcd=definition.Parse_jsonata(codeSnip=ArmcdCodeSnip,data=data)
         Arm=definition.Parse_jsonata(codeSnip=ArmCodeSnip,data=data)
         TVENRL=definition.Parse_jsonata(codeSnip=TVENRLCodeSnip ,data=data)
         TVSTRL=definition.Parse_jsonata(codeSnip=TVSTRLCodeSnip ,data=data)
+        resultVisitNum = []
         resultVisit = []
         resultVisitdy = []
         resultArmcd = []
         resultArm = []
         resultTVENRL = []
         resultTVSTRL = []
+        if VisitNum[0] == "{":  # check if the result is a list
+            definition.string_to_list(VisitNum, resultVisitNum)
         if Visit[0] == "{":  # check if the result is a list
             definition.string_to_list(Visit, resultVisit)  # convert the string to a list
         else:
@@ -93,7 +100,13 @@ def Create_TV(wb, JsonInput):
         resultarm2 = []
         resultTVENRL2 = []
         resultTVSTRL2 = []
+        VisitEncounter = []
+        NextVisit = []
 
+        for item in resultVisitNum:
+            VisitEncounterID, VisitEncounterLess = definition.get_ID(item)
+            VisitEncounter.append(VisitEncounterID)  # appending the ID to the list
+            NextVisit.append(VisitEncounterLess)  # appending the less ID to the list
         for item in resultVisit:
             VisitID, VisitLess = definition.get_ID(item)  # extracting the ID from the string
             resultvisit2.append(VisitLess)  # appending the ID to the list
@@ -112,25 +125,59 @@ def Create_TV(wb, JsonInput):
         for item in resultTVSTRL:   
             TVSTRLID, TVSTRLLess = definition.get_ID(item)  # extracting the ID from the string
             resultTVSTRL2.append(TVSTRLLess)  # appending the ID to the list
+        
+        VisitEncounter3 = []
+        resultVisit3 = []
+        resultVisitdy3 = []
+        resultTVENRL3 = []
+        resultTVSTRL3 = []
 
+        currentEncounter = None
+        for n in range(len(VisitEncounter)):
+            for i in range(len(VisitEncounter)):
+                if currentEncounter is None:
+                    if NextVisit[i] == "None":
+                        VisitEncounter3.insert(0, VisitEncounter[i])
+                        resultVisit3.insert(0, resultvisit2[i])
+                        if len(resultvisitdy2) > i:
+                            resultVisitdy3.insert(0, resultvisitdy2[i])
+                        if len(resultTVENRL2) > i:
+                            resultTVENRL3.insert(0, resultTVENRL2[i])
+                        if len(resultTVSTRL2) > i:
+                            resultTVSTRL3.insert(0, resultTVSTRL2[i])
+                        currentEncounter = VisitEncounter[i]
+                        break
+                elif NextVisit[i] == currentEncounter:
+                    VisitEncounter3.insert(0, VisitEncounter[i])
+                    resultVisit3.insert(0, resultvisit2[i])
+                    if len(resultvisitdy2) > i:
+                        resultVisitdy3.insert(0, resultvisitdy2[i])
+                    if len(resultTVENRL2) > i:
+                        resultTVENRL3.insert(0, resultTVENRL2[i])
+                    if len(resultTVSTRL2) > i:
+                        resultTVSTRL3.insert(0, resultTVSTRL2[i])
+                    currentEncounter = VisitEncounter[i]
+                    break      
+                
         for j in range(len(resultarmcd2)):
-            for i in range(len(resultvisit2)):
+            for i in range(len(resultVisit3)):
                 x += 1
                 ta_sheet.cell(row=x, column=StudyIDColumn).value = studyId
                 ta_sheet.cell(row=x, column=DomainColumn).value = DomainResult
-                if len(resultvisitdy2) > i:
-                    ta_sheet.cell(row=x, column=VisitdyColumn).value = resultvisitdy2[i]
+                if len(resultVisitdy3) > i:
+                    ta_sheet.cell(row=x, column=VisitdyColumn).value = resultVisitdy3[i]
                 else:
                     ta_sheet.cell(row=x, column=VisitdyColumn).value = " "
-                ta_sheet.cell(row=x, column=VisitDescriptionColumn).value = resultvisit2[i]
+                ta_sheet.cell(row=x, column=VisitNumColumn).value = i+1
+                ta_sheet.cell(row=x, column=VisitDescriptionColumn).value = resultVisit3[i]
                 ta_sheet.cell(row=x, column=ArmcdColumn).value = resultarmcd2[j]
                 ta_sheet.cell(row=x, column=ArmColumn).value = resultarm2[j]
-                if len(resultTVENRL2) > i:
-                    ta_sheet.cell(row=x, column=TVENRLColumn).value = resultTVENRL2[i]
+                if len(resultTVENRL3) > i:
+                    ta_sheet.cell(row=x, column=TVENRLColumn).value = resultTVENRL3[i]
                 else:
                     ta_sheet.cell(row=x, column=TVENRLColumn).value = " "
-                if len(resultTVSTRL2) > i:
-                    ta_sheet.cell(row=x, column=TVSTRLColumn).value = resultTVSTRL2[i]
+                if len(resultTVSTRL3) > i:
+                    ta_sheet.cell(row=x, column=TVSTRLColumn).value = resultTVSTRL3[i]
                 else:
                     ta_sheet.cell(row=x, column=TVSTRLColumn).value = " "
 
