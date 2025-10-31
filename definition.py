5705f9f179a24556eca1cb7585650eb1735ef05a
bsnoeijer <b.snoeijer@clinline.eu>
2025-10-31 10:52:00 +0100

    Removed HTML from eligibility

--- definition.py
+++ definition.py
@@ -1,6 +1,4 @@
 import jsonata
-import json
-import openpyxl
 import pandas as pd
 import re
 
@@ -97,16 +95,72 @@ def ResolveTag(Txt,data):
         if m:
             attrs = m.group(1)
             # print (attrs, m.end(0), m.start(1))
-            NewTxt=Get_TagLocation(attrs,data)
+            NewTxt=Get_TagValue(attrs,data)
             Txt2=Txt[0:m.start(1)-16] + str(NewTxt) + Txt[m.end(0):len(Txt)]
             # print(Txt2)
             Txt=Txt2
-
-   result=Txt
+   result=Get_plainText(Txt)
    return result   
 
 
-def Get_TagLocation(tag,data):
+def Get_plainText(TxtRich):
+    # replace <li> with -
+    TxtRich = re.sub(r'<li>(.*?)</li>', r' - \1;', TxtRich, flags=re.DOTALL | re.IGNORECASE)
+    # replace <br> with space
+    TxtRich = re.sub(r'<br\s*/?>', ' ', TxtRich, flags=re.DOTALL | re.IGNORECASE)
+    # replace <tr> with space
+    TxtRich = re.sub(r'<tr\s*/?>', ' ', TxtRich, flags=re.DOTALL | re.IGNORECASE)
+    # remove <p> and </p>
+    TxtRich = re.sub(r'</?p\s*/?>', '', TxtRich, flags=re.DOTALL | re.IGNORECASE)
+    #remove <ol> and </ol>
+    TxtRich = re.sub(r'</?ol\s*/?>', '', TxtRich, flags=re.DOTALL | re.IGNORECASE)
+    #remove <ul> and </ul>
+    TxtRich = re.sub(r'</?ul\s*/?>', '', TxtRich, flags=re.DOTALL | re.IGNORECASE)
+    # remove all other HTML tags
+    TxtRich = re.sub(r'<[^>]+>', '', TxtRich, flags=re.DOTALL | re.IGNORECASE)
+    # replace &lt; with <
+    TxtRich = TxtRich.replace('&lt;', '<')  
+    TxtRich = TxtRich.replace('&le;', '<=')  
+    # replace &gt; with >
+    TxtRich = TxtRich.replace('&gt;', '>')  
+    
+    TxtRich = TxtRich.replace('&gt;', '>=')  
+    # replace &amp; with &
+    TxtRich = TxtRich.replace('&amp;', '&')     
+    # replace &#174; with ®     
+    TxtRich = TxtRich.replace('&#174;', ' (R)')     
+    # replace &#8482; with ™    
+    TxtRich = TxtRich.replace('&#8482;', ' (TM)')     
+    # replace &#169; with © 
+    TxtRich = TxtRich.replace('&#169;', ' (C)')
+    # replace ≤ with <=
+    TxtRich = TxtRich.replace('≤', '<=')
+    
+    # collapse whitespace
+    TxtRich = re.sub(r'\s+', ' ', TxtRich).strip()
+   
+    
+    TxtRich = TxtRich.replace('\\n-', '-')
+    TxtRich = TxtRich.replace('\n-', '-')
+    # remove \n
+    TxtRich = TxtRich.replace("\\n", "")
+    # remove multiple spaces    
+    TxtRich = re.sub(' +', ' ', TxtRich)    
+
+    TxtRich = re.sub('\\n', '', TxtRich)        
+    TxtRich = re.sub('\n', '', TxtRich)   
+    
+    TxtRich = TxtRich.replace(";", "; ") 
+
+    
+    # collapse whitespace
+    TxtRich = re.sub(r'\s+', ' ', TxtRich).strip()
+   
+    print(TxtRich)
+    return TxtRich
+
+
+def Get_TagValue(tag,data):
     jsonataString = "study.versions.dictionaries.parameterMaps[tag='" + tag + "'].reference"
     expr = jsonata.Jsonata(jsonataString)
     reference = expr.evaluate(data)
